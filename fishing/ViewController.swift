df6376f37459a8a4087e92b3ba291307e2474053
yabsab <zzzhhha@gmail.com>
2016-10-12 18:59:44 +0900

    Initial Commit

---
+++ fishing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fishing
+//
+//  Created by Yazz on 2016. 10. 12..
+//  Copyright © 2016년 IMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
